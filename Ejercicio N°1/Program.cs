65b395c4aa14e8ec2336e2e7cb3fa7deb0027205
Pazos Damian <dpazos@andreani.com>
2022-02-23 22:16:50 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio N°1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_N_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Implemente la clase Alumno para modelar alumnos en una Universidad. Agréguele como estado lo que considere
+             * necesario para el problema. Piense como implementaría los horarios de cursada de un alumno? 
+             * (piense que debe guardar día, hora y materia a cursar en ese horario).
+             * Como comportamiento Implemente el método inscripcionACursada(string diahora, string materia);
+             */
+
+
+
+        }
+    }
+}
